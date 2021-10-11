a236bf492495adb2a0d09f1bbb5a5e450f9a2da9
ferdous-webdev <ferdous.webdev@gmail.com>
2021-10-11 20:53:29 +0600

    CLTypingLable removed

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -6,31 +6,27 @@
 //
 
 import UIKit
-import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: CLTypingLabel!
+    @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
 // MARK: - Animated text (Manual Process)
 
-//        titleLabel.text = ""
-//
-//        let titleText = "⚡️FlashChat"
-//        var charIndex = 0.0
-//
-//        for letter in titleText {
-//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-//                self.titleLabel.text?.append(letter)
-//            }
-//            charIndex += 1
-//        }
-        
-// MARK: - Animated text (Cocoapods process)
-        titleLabel.text = "⚡️FlashChat"
-        
+        titleLabel.text = ""
+
+        let titleText = "⚡️FlashChat"
+        var charIndex = 0.0
+
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+    
     }
 }
